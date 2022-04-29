1502ad6c341d15f6ea6ea779c6a1f97ea215f0f9
genericNight <103332071+genericNight@users.noreply.github.com>
2022-04-29 20:47:37 +0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,74 @@
-# nyhtron
-A basic database module for nodeJS using FS &amp; JSON.
+# What is NYTHRON?
+
+nythron is a json based database solution designed to work on low-level hardware.
+nythron works with basic identifiers and creates an intiutive experience, manage multiple databases in one go using one module and as simply as declaring a variable.
+
+
+# **Initilization**
+
+
+well, first install nythron running; `npm i nythron` 
+then head over to your project and define the module;
+```js
+const { Database } = require('nythron')
+```
+after that you can create a new database;
+```js
+const items = new Database('items.json')
+```
+the items variable will be used to access all the data in the items.json directory.
+ 
+
+# **Working with singular data**
+
+we will use our previous items database to give the examples.
+```js
+items.set('key', 'value') //set will overwrite the previous values.
+items.add('key', 1) //add will add a number or string.
+```
+below is whats used for fetching or getting a value
+```js
+items.get('kay')
+// will return undefined since kay does not exist.
+//so instead run;
+if (items.has('key')) {
+    items.get('kay')
+} else {
+    return 'no item to get.'
+}
+```
+
+
+# **Working with arrays**
+
+use the `.push` and `.arrayFetch` methods.
+```js
+let obj = {
+    name: "asd",
+    time: "hm"
+}
+items.push('object', obj)
+//.push acts like the .add method.
+//it will add the values that were changed and the values that werent will remain same.
+
+db.objectFetch('Object', 'key'); /* key: "value1" */
+db.arrayFetch('Array', 1); /* element2 */
+```
+
+
+# **Other methods**
+
+```js
+db.remove('data'); // Removes the data from the database
+db.delete('Array', 'element3'); // Removing something from an array using value/index
+db.deleteKey('object', 'key'); // Deletes the provided key from the given object
+db.deleteEach('data'); // Deletes each data that starts with the given parameter
+db.clear(); // Clears everything from the database
+db.destroy(); // Delete the database file (And Clear All Data)
+db.fetchAll(); // Fetches all the data in the database
+db.all(); // Fetches everything in the database
+db.objectFetch('Object', 'key'); /* key: "value1" */
+db.arrayFetch('Array', 1); /* element2 */
+db.setBackup('backup.json') // Set "backup.json" as the backup file
+db.loadBackup(); // Loads the backup from the backup file (setBackup) function
+```
---
+++ index.js
@@ -0,0 +1,298 @@
+const fs = require('fs');
+const load = (file) => JSON.parse(fs.readFileSync(file, 'utf-8')); 
+const write = (file, data) =>  fs.writeFileSync(file, JSON.stringify(data, null, 4));
+const extension = (filePath) => { let parts = filePath.split('.'); return parts[parts.length - 1]; }
+let backup;
+const Backupadd = (data) => { if(backup !== undefined) { fs.writeFileSync(backup, JSON.stringify(data, null, 4)); } }
+class Database {
+  constructor(file) {
+    this.file = file || 'database.json'
+    if(this.file === 'database.json') {
+      try { load(this.file); } catch { write(this.file, {}); }
+    } else {
+	  if (!this.file.includes('./')) this.file = './' + this.file
+      if(extension(this.file) !== 'json') throw Error("[database issue!] - the database file must be set up with the .json prefix, please change this and continue.")
+      try { load(this.file); } catch { write(this.file, {}); }
+    }
+  }
+
+  setBackup(filePath) {
+    if (!filePath) throw Error("[database issue!] a .json file must be mentioned for backups"); //err msg, tr/s4½
+    if (extension(filePath) !== "json") throw Error("[database issue!] the backup file must end with .json");
+    if (!filePath.includes('./')) filePath = './' + filePath
+    backup = filePath;
+    try { load(backup); } catch { write(backup, {}); }
+    return; //æ@€al-discord.gg/javascript
+  }
+  loadBackup() {
+    if (backup === undefined) throw Error("[database issue!] can't find a backup || a file to load");
+    write(this.file, load(backup));
+    return;
+  }
+  set(data, value) {
+    
+    if (!data) throw Error("[database issue!] no data to set");
+    if (value == 0) {
+      let fileData = load(this.file)
+      fileData[data] = value;
+      write(this.file, fileData);
+        Backupadd(fileData);
+        return;
+  
+    }
+    if (!value) throw Error("[database issue!] no value to set");
+	let fileData = load(this.file)
+	fileData[data] = value;
+	write(this.file, fileData);
+    Backupadd(fileData);
+    return;
+  } //€€₺€ißæ err, set for ❤
+  remove(data) {
+    if(!data) throw Error("[database issue!] no value to remove")
+    let fileData = load(this.file)
+    if(!fileData[data]) throw Error("[database issue!] mentioned data isn't in directory or cannot be reached")
+    fileData[data] = undefined;
+    write(this.file, fileData);
+    Backupadd(fileData);
+    return;
+  }
+  add(data, value) {
+    if (!data) throw Error("[database issue!] no data to add");
+	if (value == 0) {
+    if(typeof value == "number") {
+      let fileData = load(this.file)
+      if (fileData[data] === undefined) return this.set(data, value);
+      if(isNaN(fileData[data])) return this.set(data, value);
+      fileData[data] = fileData[data] + value;
+      write(this.file, fileData);
+        Backupadd(fileData);
+        return;
+      } else {
+      let fileData = load(this.file)
+      if (fileData[data] === undefined) return this.set(data, value);
+      if(isNaN(fileData[data])) return this.set(data, value);
+      fileData[data] = fileData[data] + value;
+      write(this.file, fileData);
+        Backupadd(fileData);
+        return;
+      }
+    }
+  
+    if (!value) throw Error("[database issue!] no value to add");
+	if(typeof value == "number") {
+	let fileData = load(this.file)
+	if (fileData[data] === undefined) return this.set(data, value);
+	if(isNaN(fileData[data])) return this.set(data, value);
+	fileData[data] = fileData[data] + value;
+	write(this.file, fileData);
+    Backupadd(fileData);
+    return;
+	} else {
+	let fileData = load(this.file)
+	if (fileData[data] === undefined) return this.set(data, value);
+	if(isNaN(fileData[data])) return this.set(data, value);
+	fileData[data] = fileData[data] + value;
+	write(this.file, fileData);
+    Backupadd(fileData);
+    return;
+	}
+  }
+
+
+ //made with ❤ by pythonic
+  subtract(data, value) {
+    if (!data) throw Error("[database issue!] No data to subtract");
+    if (!value) throw Error("[database issue!] No value to subtract");
+    if (typeof value !== "number") throw Error(`[database issue!] The value to substract must be a number, received type: ${typeof value}`);
+    let fileData = load(this.file)
+    if (file[data] === undefined) return this.set(data, value);
+    if(isNaN(file[data])) return this.set(data, value);
+    fileData[data] = fileData[data] - value;
+    write(this.file, fileData);
+    Backupadd(fileData);
+    return;
+  }
+
+
+                   //🪐 this is a nice planet.
+  deleteEach(data) {
+    if (!data) throw Error("[database issue!] No data to deleteEach")
+    let fileData = load(this.file)
+    let item = Object.keys(fileData)
+    if (item === '') throw Error(nothingToDeleteeach)
+    item = item.filter((Data) => Data.includes(data));
+    item.forEach((Data) => {
+      this.remove(Data)
+    });
+    return;
+  }
+
+
+ // 🛒 marketede giderim ben sanane
+  push(array, value) {
+    if (!array) throw Error("[database issue!] No array to push")
+    if (!value) throw Error("[database issue!] No value to push to the array")
+    let fileData = load(this.file)
+    if (fileData[array] && Array.isArray(fileData[array])) {
+      fileData[array].push(value)
+      write(this.file, fileData)
+    } else if (!fileData[array]) {
+      this.set(array, [value])
+    }
+    return
+  }
+
+
+ //db.map shall b€ inclU€d for v2.
+
+  delete(array, index) {
+    if (!array) throw Error("[database issue!] No array to index/value delete")
+    if (index === undefined) throw Error("[database issue!] No index/value to delete from the array")
+    let fileData = load(this.file)
+    if (!fileData[array] && !Array.isArray(fileData[array])) throw Error("[database issue!] The array to index/value delete dosen't exist or it's not array")
+    if (typeof index === "number") {
+      fileData[array].splice(index, 1)
+      write(this.file, fileData)
+      } else if(isNaN(index)) {
+        if(fileData[array].includes(index)) {
+          fileData[array].splice(fileData[array].indexOf(index), 1)
+          write(this.file, fileData)
+       } else { throw Error("[database issue!] Unable to find a value with the provided index/value to delete"); }
+      }
+      return;
+  }
+
+
+
+ //<html> forKey: "all-destroy" </html>
+  deleteKey(object, key) {
+    if (!object) throw Error("[database issue!] No object to key delete");
+    if (!key) throw Error("[database issue!] No key to delete from the object");
+    let fileData = load(this.file);
+    if (!fileData[object]) throw Error("[database issue!] The object to delete key dosen't exist in the database");
+    if (typeof fileData[object] !== 'object') throw Error("[database issue!] The provided object to key delete is not an object in the database");
+    delete fileData[object][key];
+    write(this.file, fileData);
+    return;
+  }
+
+
+//Code obsidestructor ⚔
+
+  has(data) {
+    if (!data) throw Error("[database issue!] No data to has function");
+    let fileData = load(this.file)
+    if (!fileData[data]) return false;
+    if (fileData[data]) return true;
+  }
+
+
+//do not, delete the code below - - - discor
+//--------------------------------\\
+/* 
+Welcome, hope you are enjoying this package
+this package was created for discord.gg/javascript
+and for anyone's use. its open source and free for use
+please credit us.
+❤
+*/
+//--------------------------------\\
+
+  clear() {
+    write(this.file, {});
+    return;
+  }
+
+
+
+
+  fetchAll() {
+    return load(this.file);
+  }
+
+
+
+
+  all() {
+    return load(this.file);
+  }
+
+
+  destroy() {
+    fs.unlinkSync(this.file);
+    return;
+  }
+
+
+
+  fetch(data) {
+    if (!data) throw Error("[database issue!] No data to fetch")
+    let fileData = load(this.file)
+    if(!fileData[data]) fileData[data] = null
+    return fileData[data]
+  }
+
+
+
+  get(data) {
+    if (!data) throw Error("[database issue!] No data to get")
+    let fileData = load(this.file)
+    if(fileData[data] == 0) {
+      fileData[data] = 0
+      return fileData[data]
+    }
+    if(!fileData[data]) fileData[data] = null
+    return fileData[data]
+  }
+
+
+
+  objectFetch(object, key) {
+    let fileData = load(this.file)
+    if (!object) throw Error("[database issue!] No object to object fetch")
+    if (!key) throw Error("[database issue!] No key to object fetch")
+    if (!fileData[object]) throw Error("[database issue!] The object to object fetch dosen't exist in the database");
+    if (typeof fileData[object] !== 'object') throw Error("[database issue!] The provided object to object fetch is not an object in the database");
+    if(!fileData[object][key]) fileData[object][key] = null
+    return fileData[object][key]
+  }
+
+
+
+  arrayFetch(array, number) {
+    let fileData = load(this.file)
+    if (!array) throw Error("[database issue!] No array to array fetch")
+    if (!number && number != 0) throw Error("[database issue!] No index/number to array fetch")
+    if (!fileData[array] && !Array.isArray(fileData[array])) throw Error("[database issue!] The array to fetch dosen't exist or it's not array")
+    if (typeof number !== "number" && value !== 0) throw Error(`[database issue!] The number/index to array fetch must be a number, received type: ${typeof value}`);
+    if(!fileData[array][number]) fileData[array][number] = null
+    return fileData[array][number];
+  }
+
+
+
+  math(data, operator, value) {
+    if (!data) throw Error("[database issue!] No data to math")
+    if (!operator) throw Error("[database issue!] No operator to math")
+    if (!value) throw Error("[database issue!] No value to math")
+    if (typeof value !== "number") throw Error(`[database issue!] The value to math must be a number, received type: ${typeof value}`);
+    let fileData = load(this.file)
+    if (operator === "-") {
+      let i = fileData[data] - value;
+      return i
+    } else if (operator === "+") {
+      let ii = fileData[data] + value;
+      return ii
+    } else if (operator === "*") {
+      let iii = fileData[data] * value;
+      return iii
+    } else if (operator === "/") {
+      let iiii = fileData[data] / value;
+      return iiii
+    } else {
+      throw Error("[database issue!] unvaild operator to math, you can use only (-, +, *, /) (math) function")
+    }
+  }
+}
+
+module.exports = { Database };
